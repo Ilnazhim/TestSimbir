be370f598ec736683ac7974fd1cb1aeca5a0d505
iLnazhim <bass_boss@mail.ru>
2024-02-18 17:11:38 +0500

    Change base class

--- base/base_class.py
+++ base/base_class.py
@@ -25,46 +25,12 @@ class BaseClass:
         assert value_word == result
         print("Good value word")
 
-    def is_element_present(self, how, what):
-        """Metod is element present"""
-        try:
-            WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable((how, what)))
-            print("Assert element is present")
-        except NoSuchElementException:
-            return False
-        return True
-
-    def is_not_element_present(self, how, what, timeout=1):
-        try:
-            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
-        except TimeoutException:
-            return True
-
-        return False
-
-    def is_disappeared(self, how, what, timeout=4):
-        try:
-            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
-                until_not(EC.presence_of_element_located((how, what)))
-        except TimeoutException:
-            return False
-
-        return True
-
-
     def get_screenshot(self):
         """Metod screenshot"""
         now_date = datetime.datetime.utcnow().strftime("%Y.%m.%d.%H.%M.%S")
         name_screenshot = 'screenshot' + now_date + '.png'
         self.browser.save_screenshot("C:\\Users\\ilnazhim\\environments\\StepikAlexFinishProject\\screen\\" + name_screenshot)
         print("Screenshot")
-
-
-    def assert_url(self, result):
-        """Metod assert url"""
-        get_url = self.browser.current_url
-        assert get_url == result
-        print("Good value url")
 
     def return_amount():
         date_now = int(datetime.datetime.utcnow().strftime('%d')) + 1
@@ -74,5 +40,3 @@ class BaseClass:
             else:
                 return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
         return fibonacci_recursive(date_now)
-
-    print(return_amount())
